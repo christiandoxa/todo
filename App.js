cb4ed5a0cf8cd5115b14708f6cfcea0bae1a801d
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2019-02-05 05:16:19 +0700

    - finished project

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, Text, TextInput, TouchableOpacity, View} from 'react-native';
 
-export default class App extends React.Component {
+class App extends React.Component {
+    state = {
+        text: "",
+        todo: []
+    };
+
+    addTodo = () => {
+        const newTodo = this.state.text;
+        const arr = this.state.todo;
+        arr.push(newTodo);
+        this.setState({todo: arr, text: ''});
+    };
+
+    deleteTodo = (t) => {
+        const deleteTodo = this.state.todo.filter(todo => todo !== t);
+        this.setState({todo: deleteTodo});
+    };
+
+    renderTodos = () => {
+        return this.state.todo.map(t => {
+            return (
+                <TouchableOpacity key={t}>
+                    <Text style={styles.todo}
+                          onPress={() => {
+                              this.deleteTodo(t)
+                          }}>{t}</Text>
+                </TouchableOpacity>
+            );
+        });
+    };
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
+            <View style={styles.wholeStyle}>
+                <View style={styles.viewStyle}>
+                    <Text style={styles.header}>Notes App</Text>
+                    <TextInput
+                        style={styles.inputStyle}
+                        onChangeText={text => this.setState({text})}
+                        value={this.state.text}
+                    />
+                    <Button
+                        title="Add Todo"
+                        color="white"
+                        onPress={this.addTodo}/>
+                    <View style={{marginTop: 100}}/>
+                    {this.renderTodos()}
+                </View>
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+const
+    styles = {
+        wholeStyle: {
+            backgroundColor: "#0288D1",
+            flex: 1
+        },
+        viewStyle: {
+            marginTop: 30,
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: 10,
+        },
+        inputStyle: {
+            height: 40,
+            width: '100%',
+            borderColor: 'white',
+            borderWidth: 1
+        },
+        header: {
+            fontSize: 30,
+            color: 'white',
+            fontWeight: 'bold'
+        },
+        todo: {
+            fontSize: 24,
+            color: 'white'
+        }
+    };
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,13 @@
+# todo
+Aplikasi react native untuk mencatat list aktivitas
+
+## Screenshot Aplikasi
+<p align="center"> 
+  <img src="https://drive.google.com/uc?id=1Yv-pjl0KLb3MCD6zgd8flp6dUYAf5sz3" width="40%"/>
+  <img src="https://drive.google.com/uc?id=1hwqgcwyHgP9E-prtdlQUc6kzv5m6XV0I" width="40%"/>
+</p>
+
+### Creator
+<a href="https://linkedin.com/in/christiandoxa/">
+Christian Doxa Hamasiah
+</a>
